3573962e3162b64e3d6f1eb01771f96608300d59
F-A-Sweet <baust.ferdousahmmed@gmail.com>
2026-07-13 01:47:37 +0600

    Enhance BasePage with explicit wait

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,10 +1,20 @@
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
 class BasePage:
 
     def __init__(self, driver):
         self.driver = driver
 
     def click(self, locator):
-        self.driver.find_element(*locator).click()
+        self.wait_for_element(locator).click()
 
     def type(self, locator, text):
-        self.driver.find_element(*locator).send_keys(text)
+        element = self.wait_for_element(locator)
+        element.clear()
+        element.send_keys(text)
+
+    def wait_for_element(self, locator, timeout=10):
+        return WebDriverWait(self.driver, timeout).until(
+            EC.visibility_of_element_located(locator)
+        )
